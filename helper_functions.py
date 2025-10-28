93a983f180e5eff17b6976d2ce3f6b922166627e
Toomuchtosay <tomuchtosay16@gmail.com>
2025-10-28 10:24:00 -0400

    M feature: randomly selects from directory of available sounds for waiting

--- helper_functions.py
+++ helper_functions.py
@@ -5,7 +5,7 @@ import pygame
 import sys
 from os import listdir
 from ascii_art import cat
-
+from random import randint
 def get_alarm_list():
     choices = listdir("sounds")
     for number in range(0, len(choices)):
@@ -35,8 +35,10 @@ def convert_time(time): #THIS IS CALLED FOR BOTH THE START TIME AND THE END TIME
 
 def track_time(start_time, time_limit):
     #block of code that hardcodes the song you want to play while the time is going
+    song_options = listdir['~/home/jacob/alarm_clock/sounds']
+    random = randoppiint(0, len(song_options))
     pygame.mixer.init()
-    pygame.mixer.music.load('sounds/SubwooferLullaby.mp3')
+    pygame.mixer.music.load(f'{song_options[random]}')
     pygame.mixer.music.play(loops=-1)
     #end of block#
     time = convert_time(time_limit)
